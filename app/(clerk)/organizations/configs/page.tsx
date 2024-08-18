2f7705a658bab812d0fe8f82438690d502cfcd7d
shehab ayman <shehabayman355@gmail.com>
2024-08-18 14:39:09 +0300

    add: update commit

---
+++ app/(clerk)/organizations/configs/page.tsx
@@ -0,0 +1,8 @@
+import { CardForm } from "@/components/page-structure/CardForm";
+
+const Configs = () => {
+    return <CardForm heading="Configs">Configs</CardForm>;
+};
+
+Configs.displayName = "Configs";
+export default Configs;
