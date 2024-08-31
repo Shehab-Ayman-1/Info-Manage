898219ca0b2fe8b532c7bac3a4068a9027e2ad97
shehab ayman <shehabayman355@gmail.com>
2024-08-31 07:05:27 +0300

    add: update commit

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,18 @@
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
+import fs from "fs";
+import path from "path";
 
 export default getRequestConfig(async () => {
     const cookieLocale = cookies().get("NEXT_LOCALE")?.value;
 
     const locale = cookieLocale || "en";
 
-    const files = ["buttons", "dialogs", "footer", "header", "overview", "pages", "public", "subscriptions", "table"];
+    const dir = path.join("i18n", "messages", locale);
+    const files = fs.readdirSync(dir);
 
     const messages = await files.reduce(async (acc, file) => {
-        const fileContents = (await import(`@/i18n/messages/${locale}/${file}.json`)).default;
+        const fileContents = (await import(`@/i18n/messages/${locale}/${file}`)).default;
         const previousValues = await acc;
         return { ...previousValues, ...fileContents };
     }, {});
